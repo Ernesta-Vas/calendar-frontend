88d4b997d2a85871cb2370a0f522040de937fea6
ErnestaVasilieva <erna.vas2002@gmail.com>
2024-07-14 12:26:28 +0300

    изменен vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'calendar-frontend'
+  base: 'calendar-frontend',
+  define: {
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+  },
 })
