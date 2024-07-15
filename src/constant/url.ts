e3e7a4340121ee9029087a4d50443bbc3d3530d8
ErnestaVasilieva <erna.vas2002@gmail.com>
2024-07-15 14:08:50 +0300

    исправлен продакшен урл

--- src/constant/url.ts
+++ src/constant/url.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? 'http://31.129.63.84' : 'http://localhost:8080';
+export const API_URL = process.env.NODE_ENV === 'production' ? 'http://31.129.63.84:8080' : 'http://localhost:8080';
 
